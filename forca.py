c9352784a06a3e780ce0e0261708b5fe738f431f
AtilaMedeiros <atila.medeiros@gmail.com>
2020-10-07 16:46:33 -0300

    updating forca.py

--- forca.py
+++ forca.py
@@ -1,8 +1,22 @@
-
 def jogar():
     print("***************************")
     print("Bem vindo ao jogo de Forca!")
     print("***************************")
+
+    palavra_chave = "banana"
+
+    acertou = False
+    enforcou = False
+
+    while (not acertou and not enforcou):
+        chute = input("Qual letra?")
+
+        index = 0
+        for letra in palavra_chave:
+            if (chute == letra):
+                print(f"Encontrei a letra {chute} na posição {index} ")
+
+            index += 1
 
 
 if (__name__ == "__main__"):
